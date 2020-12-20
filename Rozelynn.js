cf3a482a797b45e99f2c3bbbc13504f41ac88881
roselynn411 <75948043+roselynn411@users.noreply.github.com>
2020-12-19 23:33:47 -0500

    updated file

--- Rozelynn.js
+++ Rozelynn.js
@@ -1,5 +1,21 @@
-.DARKMODE{
-Background-color: blue;
-Color:white;
+var darkModeButton = document.getElementById('mybutton');
+var webpageBackground = document.body;
+var hideDarkMode = false;
+var selfPortrait = document.getElementById('darkModeButton');
 
+function activate() {
+  if (hideDarkMode == true) {
+    webpageBackground.style.background='#b8c6db';
+     hideDarkMode = false;
+     darkModeButton.innerHTML='Apply Dark Mode';
+
+  }
+  else if (hideDarkMode == false) {
+    webpageBackground.style.background='black';
+    hideDarkMode = true;
+    darkModeButton.innerHTML='Hide Dark Mode';
+
+  }
+  console.log("activate");
 }
+darkModeButton.addEventListener("click", activate);
